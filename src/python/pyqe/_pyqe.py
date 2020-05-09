1a707812e7d54f55c2f3fcf8c543675cdb649bb0
Max Radin <radin.max@gmail.com>
2020-05-09 01:40:40 -0400

    Arrays now broken down into tables

--- src/python/pyqe/_pyqe.py
+++ src/python/pyqe/_pyqe.py
@@ -3,24 +3,58 @@ import pandas as pd
 from flatten_json import flatten
 from sqlalchemy import create_engine
 
-def extract_tasks(workflowresult):
+def extract_lists(item, dataset, path=None, index_buffer=(), parent=None):
+    if isinstance(item, dict):
+        if len(index_buffer) > 0:
+            dataset[path].append(item)
 
-    tasks = {}
+        to_pop = []
+        for key in item:
+            if path:
+                new_path = path + '.' + key
+            else:
+                new_path = key
+            extract_lists(item[key], dataset, new_path, (), item)
+            if isinstance(item[key], list):
+                to_pop.append(key)
+        for key in to_pop:
+                item.pop(key)
+
+    elif isinstance(item, list):
+        if not dataset.get(path):
+            dataset[path] = []
+        for index, child in enumerate(item):
+            extract_lists(child, dataset, path, index_buffer + (index,), parent)
+
+    elif len(index_buffer) > 0:
+        item_as_dict = {'value': item}
+        item_as_dict.update({f'index_{i}': j for i, j in enumerate(index_buffer)})     
+        item_as_dict['parentId'] = parent.get('id')   
+        dataset[path].append(item_as_dict)
+
+def get_class_dict(workflowresult):
+    class_dict = {}
+
     for step in workflowresult:
         step_class = workflowresult[step]['class']
-        if not tasks.get(step_class):
-            tasks[step_class] = []
-        
-        tasks[step_class].append(workflowresult[step])
-    return tasks
+        if not class_dict.get(step_class):
+            class_dict[step_class] = []
+        class_dict[step_class].append(workflowresult[step])
+    return class_dict
+
+def get_super_dict(workflowresult):
+    super_dict = get_class_dict(workflowresult)
+    children = {}
+    extract_lists(super_dict, children)
+    super_dict.update(children)
+    return super_dict
 
 def extract_dataframes(workflowresult):
-    tasks = extract_tasks(workflowresult)
+    super_dict = get_super_dict(workflowresult)
     dfs = {}
-    for task in tasks:
-
-        dict_flattened = (flatten(step, '.') for step in tasks[task])
-        dfs[task] = pd.DataFrame(dict_flattened)
+    for table_name in super_dict:
+        dict_flattened = (flatten(item, '.') for item in super_dict[table_name])
+        dfs[table_name] = pd.DataFrame(dict_flattened)
     return dfs
 
 def send_workflowresult_to_sql(workflowresult):
--- src/python/pyqe/_pyqe_test.py
+++ src/python/pyqe/_pyqe_test.py
@@ -1,14 +1,58 @@
-from _pyqe import extract_tasks, extract_dataframes, send_workflowresult_to_sql
+import unittest
+from _pyqe import (extract_tasks, extract_dataframes,
+    send_workflowresult_to_sql, extract_lists)
 import json
 
 
+class TestPyqe(unittest.TestCase):
 
-with open('/Users/maxradin/science/vqe-tutorial-prebaked/49e36f22-9295-5488-af33-cabe24760799.json') as f:
-    workflowresult = json.load(f)
-tasks = extract_tasks(workflowresult)
-print(tasks)
+    def test_(self):
+        task_data_1 = {
+            "id" : 0,
+            "two_dimensional_array" : [[1, -1], [2, -2], [3, -3]],
+            "scalar" : 1,
+            "one_dimensional_array" : [1, 2, 3],
+            "list_of_dicts" : [{"a": 1, "b": 2}, {"a": 3, "b": 4}]
+        }
+        task_data_2 = {
+            "id" : 0,
+            "two_dimensional_array" : [[1, -1], [2, -2], [3, -3]],
+            "scalar" : 1,
+            "one_dimensional_array" : [1, 2, 3],
+            "list_of_dicts" : [{"a": 1, "b": 2}, {"a": 3, "b": 4}]
+        }
+        children = {}
+        extract_lists(task_data_1, children)
+        extract_lists(task_data_2, children)
 
-dfs = extract_dataframes(workflowresult)
-print(dfs)
+        print(json.dumps(children, indent=2))
 
-send_workflowresult_to_sql(workflowresult)
+        self.assertTrue(len(children['two_dimensional_array']), 8)
+        self.assertFalse(task_data_1.get('two_dimensional_array'))
+        self.assertFalse(task_data_2.get('two_dimensional_array'))
+
+        self.assertTrue(len(children['one_dimensional_array']), 6)
+        self.assertFalse(task_data_1.get('one_dimensional_array'))
+        self.assertFalse(task_data_2.get('one_dimensional_array'))
+
+        self.assertEqual(task_data_1['scalar'], 1)
+        self.assertEqual(task_data_2['scalar'], 1)
+
+        self.assertTrue(len(children['list_of_dicts']), 4)
+        self.assertFalse(task_data_1.get('list_of_dicts'))
+        self.assertFalse(task_data_2.get('list_of_dicts'))
+
+if __name__ == '__main__':
+    unittest.main()
+
+# 
+
+# with open('/Users/maxradin/science/vqe-tutorial-prebaked/49e36f22-9295-5488-af33-cabe24760799.json') as f:
+#     workflowresult = json.load(f)
+# tasks = extract_tasks(workflowresult)
+# print(tasks)
+
+# dfs = extract_dataframes(workflowresult)
+# print(dfs)
+
+# send_workflowresult_to_sql(workflowresult)
