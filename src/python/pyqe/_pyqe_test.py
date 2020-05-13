f52a2d882ec034cc64456d28a45c8f685e5c4998
Max Radin <radin.max@gmail.com>
2020-05-13 19:19:42 -0400

    Adding more unit tests

--- src/python/pyqe/_pyqe_test.py
+++ src/python/pyqe/_pyqe_test.py
@@ -1,46 +1,65 @@
 import unittest
-from _pyqe import (extract_tasks, extract_dataframes,
+from _pyqe import (extract_dataframes, get_super_dict, get_class_dict,
     send_workflowresult_to_sql, extract_lists)
 import json
 
 
 class TestPyqe(unittest.TestCase):
 
-    def test_(self):
-        task_data_1 = {
+    def setUp(self):
+        self.workflowresult = {}
+        self.workflowresult["step-1"] = {
+            "class" : "class-A",
             "id" : 0,
             "two_dimensional_array" : [[1, -1], [2, -2], [3, -3]],
             "scalar" : 1,
             "one_dimensional_array" : [1, 2, 3],
             "list_of_dicts" : [{"a": 1, "b": 2}, {"a": 3, "b": 4}]
         }
-        task_data_2 = {
+        self.workflowresult["step-2"] = {
+            "class" : "class-B",
             "id" : 0,
             "two_dimensional_array" : [[1, -1], [2, -2], [3, -3]],
             "scalar" : 1,
             "one_dimensional_array" : [1, 2, 3],
             "list_of_dicts" : [{"a": 1, "b": 2}, {"a": 3, "b": 4}]
         }
+
+    def test_extract_lists(self):
+
         children = {}
-        extract_lists(task_data_1, children)
-        extract_lists(task_data_2, children)
+        extract_lists(self.workflowresult["step-1"], children)
+        extract_lists(self.workflowresult["step-2"], children)
 
         print(json.dumps(children, indent=2))
 
         self.assertTrue(len(children['two_dimensional_array']), 8)
-        self.assertFalse(task_data_1.get('two_dimensional_array'))
-        self.assertFalse(task_data_2.get('two_dimensional_array'))
+        self.assertFalse(self.workflowresult["step-1"].get('two_dimensional_array'))
+        self.assertFalse(self.workflowresult["step-2"].get('two_dimensional_array'))
 
         self.assertTrue(len(children['one_dimensional_array']), 6)
-        self.assertFalse(task_data_1.get('one_dimensional_array'))
-        self.assertFalse(task_data_2.get('one_dimensional_array'))
+        self.assertFalse(self.workflowresult["step-1"].get('one_dimensional_array'))
+        self.assertFalse(self.workflowresult["step-2"].get('one_dimensional_array'))
 
-        self.assertEqual(task_data_1['scalar'], 1)
-        self.assertEqual(task_data_2['scalar'], 1)
+        self.assertEqual(self.workflowresult["step-1"]['scalar'], 1)
+        self.assertEqual(self.workflowresult["step-2"]['scalar'], 1)
 
         self.assertTrue(len(children['list_of_dicts']), 4)
-        self.assertFalse(task_data_1.get('list_of_dicts'))
-        self.assertFalse(task_data_2.get('list_of_dicts'))
+        self.assertFalse(self.workflowresult["step-1"].get('list_of_dicts'))
+        self.assertFalse(self.workflowresult["step-2"].get('list_of_dicts'))
+
+    def test_get_class_dict(self):
+        class_dict = get_class_dict(self.workflowresult)
+        self.assertEqual(len(class_dict), 2)
+        self.assertIn('class-A', class_dict)
+        self.assertIn('class-B', class_dict)
+
+    def test_get_super_dict(self):
+        super_dict = get_super_dict(self.workflowresult)
+        self.assertEqual(len(super_dict), 5)
+        for key in ('class-A', 'class-B', 'one_dimensional_array',
+                'two_dimensional_array', 'list_of_dicts'):
+            self.assertIn(key, super_dict)
 
 if __name__ == '__main__':
     unittest.main()
